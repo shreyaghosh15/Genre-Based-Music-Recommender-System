4757c52b52d40b5f11fa5cf78f3221e1d802b156
Shreya Ghosh <69425502+shreyaghosh15@users.noreply.github.com>
2021-07-01 14:35:11 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -7,14 +7,29 @@ import requests
 import IPython.display as disp
 import sklearn
 from sklearn.decomposition import TruncatedSVD
+import time
 
-st.header("Welcome To Our")
-st.title("Genre-Based Music Recommendation System")
+from streamlit.caching import suppress_cached_st_function_warning
+
+st.markdown(
+"""
+<style>
+.reportview-container{
+        background: linear-gradient(#083513 0%,#000);
+    }
+</style>
+"""
+,unsafe_allow_html=True
+)
+st.markdown("<h1 style='text-align: center; color: #1db945;'>Get Songs Recommended To Your Choice</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: #BEFFCF ; font-style: italic'> <b> Know Your Vibe.</b></h2>", unsafe_allow_html=True)
+
 songs_df= pd.read_csv('1mil-songs.csv')
-songs_df2= songs_df.loc[1:50000]
+
 song_user_count= pd.read_table('triplets.txt')
 song_user_count.columns= ['user_id','song_id','listen_count']
-merged_df= pd.merge(song_user_count, songs_df2, on='song_id')
+
+merged_df= pd.merge(song_user_count, songs_df, on='song_id')
 
 pt_df= merged_df.pivot_table(values='listen_count',index='user_id',columns='title',fill_value=0)
 
@@ -28,18 +43,20 @@ corelation_m= np.corrcoef(result_matrix)
 songnames= pt_df.columns
 songlist= list (songnames)
 
-st.header("Hey there!")
-st.subheader("Need some music recommendations based on songs you already love?")
-st.subheader("Enter a song below & we'll try our best to recommend you similar songs!")
-inp = st.text_input("")
+inp = st.text_input("Enter your song")
 
+st.cache()
 if st.button("Recommend"):
-    if (inp in songlist):
-        st.write("*Here are the songs recommended for you:*")
-        recsong = corelation_m[songlist.index(inp)]
-        st.write(list(songnames[(recsong <1.0) & (recsong >0.9)]))
         
+    if (inp in songlist):
+        my_bar= st.progress(0)
+        for percent_complete in range(100):
+            time.sleep(0.1)
+            my_bar.progress(percent_complete+1)
+            suppress_cached_st_function_warning = True
+        st.write("*Here are the songs recommended for you:* :sunglasses:")
+        recsong = corelation_m[songlist.index(inp)]
+        x=(list(songnames[(recsong <1.0) & (recsong >0.9)]))
+        st.table(x)
     else:
-        st.write("Sorry :( The song you entered is not in our database! Maybe try another?")
-
-    st.subheader("Hope you enjoy these songs!")
+        st.error("Sorry, this song isn't on our list :broken_heart: Maybe try another?")
